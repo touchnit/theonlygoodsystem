b629bbaf6e4fca6df799a436c3816d78f05d8663
touchnit <marc.abadie64@gmail.com>
2019-04-01 15:52:20 +0200

    text content added

--- script.js
+++ script.js
@@ -4,50 +4,34 @@ let presseWindow = document.getElementById("presse"),
     headerLogo = document.getElementById("header-squaring");
     var scrollPercent = document.documentElement.scrollTop;
 
-
 function openPage(pageName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-
-  // Remove the background color of all tablinks/buttons
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].classList.remove("selected");
   }
-
-  // Show the specific tab content
   document.getElementById(pageName).style.display = "block";
-
-  // Add the specific color to the button used to open the tab content
   elmnt.classList.add("selected");
 }
-
 function openImpressum(){
     impressumWindow.style.display = "block"
 }
 function openPresse(){
     presseWindow.style.display = "block"
 }
-
 function closeWindow(){
     impressumWindow.style.display = "none"
     presseWindow.style.display = "none"
 }
-
 headerLogo.addEventListener("click", clickLogo);
 
 function clickLogo(){
         document.getElementById("content").style.display = "flex"
         headerLogo.classList.remove("big-logo")
         headerLogo.classList.add("small-logo")
-        //headerLogo.setAttribute("height", "18%")
-        //headerLogo.setAttribute("width", "18%")
         setTimeout(document.getElementById("content").classList.add("opacity-on"), 1000);
 }
-
-// Get the element with id="defaultOpen" and click on it
-//document.getElementById("defaultOpen").click();
